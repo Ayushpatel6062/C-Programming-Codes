0c9d3b21ca2060b49b6458c051e1995b73316900
Ayushpatel_6062 <89897529+Ayushpatel6062@users.noreply.github.com>
2021-10-20 20:21:14 +0530

    1st File uploaded

---
+++ 1)Introduction_to_C.c
@@ -0,0 +1,9 @@
+#include<stdio.h> // 'stdio' meaning standard input and output.
+void main() // void means empty. 
+{ 
+	printf("Hello World"); // %d (integer), %f (float), %s (string), %c (character) are called as format specifiers.
+	printf("\n%d + %d = %d",6,1,6+1);
+	printf("\n%.1f / %.1f = %.0f",7.5,7.5,7.5/7.5);
+	printf("\n%.1f * %.1d = %.0f",4.5,2,4.5*2);
+	printf("\n%d - %d = %d",5,4,5-4);
+}
