047199f0f8e70529f167fd5318a0240cbd06e23e
Ayushpatel_6062 <89897529+Ayushpatel6062@users.noreply.github.com>
2021-11-23 08:55:41 +0530

    Update 1)Introduction_to_C.c

--- 1)Introduction_to_C.c
+++ 1)Introduction_to_C.c
@@ -1,5 +1,5 @@
 #include<stdio.h> // 'stdio' meaning standard input and output.
-void main() // void means empty. 
+void main() // void means empty. All the functions are kept in 'main' of void main
 { 
 	printf("Hello World"); // %d (integer), %f (float), %s (string), %c (character) are called as format specifiers.
 	printf("\n%d + %d = %d",6,1,6+1);
